2e692401aeb44f8163f63d284cc9f3dfa6fc65b6
Matthew Pak <mattpak@gmail.com>
2022-03-15 16:17:22 -0700

    Completed callbackReview, promiseConstructor

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,28 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  //console.log('this is the filepath:', filePath);
+  fs.readFile(filePath, 'utf8', (err, text) => {
+    if (err) {
+      callback(err);
+    } else {
+      let firstLine = text.split('\n')[0];
+      callback(null, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request.get(url, (err, res, body) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -8,15 +8,33 @@ var fs = require('fs');
 var request = require('needle');
 var Promise = require('bluebird');
 
-// This function should retrieve the first line of the file at `filePath`
+
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    // async function
+    fs.readFile(filePath, 'utf8', (err, text) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(text.split('\n')[0]);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.statusCode);
+      }
+    });
+  });
 };
+
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
